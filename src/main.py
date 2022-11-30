2e12264195bb8081eadd8ee00a636a27c257a9ad
craig.opie-v <craig.opie-v@darkwolfsolutions.com>
2022-11-30 22:59:45 +0000

    updated argument description

--- src/main.py
+++ src/main.py
@@ -48,14 +48,11 @@ class Scraper:
         self.WINDOWS_PLATFORM = windows
         self.sources = {
             "hackerone": "https://hackerone.com/hacktivity?querystring=&filter=type:public&order_direction=DESC&order_field=popular&followed_only=false&collaboration_only=false",
-            "bugcrowd": "https://bugcrowd.com/crowdstream?filter=disclosures",
         }
-        self.source = source
         self.url = str(self.sources[source])
-        if (source == "hackerone"):
-            if (key_word != ''): self.url = str(self.sources[source].replace('=&', '= &').replace(' ', key_word))
-            if (order != 'popular'): self.url = str(self.sources[source].replace('popular', 'latest_disclosable_activity_at'))
-            if (type != 'public'): self.url = str(self.sources[source].replace('public', type))
+        if (key_word != ''): self.url = str(self.sources[source].replace('=&', '= &').replace(' ', key_word))
+        if (order != 'popular'): self.url = str(self.sources[source].replace('popular', 'latest_disclosable_activity_at'))
+        if (type != 'public'): self.url = str(self.sources[source].replace('public', type))
 
         self.duration = float(duration)
         self.filename = filename
@@ -110,62 +107,61 @@ class Scraper:
         sleep(1)
         self.DATABASE['reports'] = []
 
-        if (self.source == "hackerone"):
-            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-            items = soup.find_all('div', class_='hacktivity-item')
-            for item in items:
-                _report = {}
-                index = 0
-                title = ""
-                report_number = ""
-                url = ""
-                cve = ""
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        items = soup.find_all('div', class_='hacktivity-item')
+        for item in items:
+            _report = {}
+            index = 0
+            title = ""
+            report_number = ""
+            url = ""
+            cve = ""
 
-                ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
-                tags = str(item.find('a', class_='spec-hacktivity-item-title')).replace('<a class=\"daisy-link routerlink daisy-link hacktivity-item__publicly-disclosed spec-hacktivity-item-title\" href=\"', '').replace('\">', '').replace('</a>', '').replace('</strong>', '').split('<strong>')
-                for tag in tags:
-                    if index == 1:
-                        title = str(tag).strip().replace(',', '')
-                    else:
-                        report_number = str(tag).split('/')[-1].replace(',', '')
-                        url = str('https://hackerone.com' + tag)
-                    index += 1
-
-                if ('CVE-' in title):
-                    cve = title.split('CVE-')[1].split(' ')[0].replace(':', '').replace(')', '')
-                    cve = 'CVE-' + cve
-                
-                ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
-                rating = str(item.find('div', class_='spec-severity-rating')).replace('<div class=\"sc-bcXHqe NcSfA daisy-severity-label spec-severity-rating\">', '').replace('</div>', '')
-
-                ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
-                bounty = str(item.find('strong', class_='spec-hacktivity-item-bounty')).replace('<strong class=\"spec-hacktivity-item-bounty\">', '').replace('</strong>', '').replace(',', '')
-                if str(bounty) == '':
-                    bounty = '0.00'
-
-                ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
-                upvotes = str(item.find('span', class_='inline-help')).replace('<span class=\"inline-help\" style=\"display: inline-flex;\">', '').replace('</span>', '').replace(',', '')
-                if str(upvotes) == '':
-                    upvotes = '0'
-
-                ## Publish the information for disclosed reports
-                if title != '':
-                    _report['title'] = str(title)
-                    _report['report number'] = str(report_number).replace('" rel="" target="', '')
-                    _report['url'] = str(url)
-                    _report['severity rating'] = rating
-                    _report['bounty'] = str(bounty).replace('$', '').replace(',', '')
-                    _report['upvotes'] = str(upvotes)
-
-                    self.CONN = sql.connect('h1.db')
-                    self.DATABASE['reports'].append(_report)
-                    self.CONN.execute("INSERT INTO REPORTS (TITLE,REPORT_NUMBER,URL,SEVERITY_RATING,CVE,BOUNTY,UPVOTES) VALUES (?, ?, ?, ?, ?, ?, ?)", (str(title), str(report_number), str(url), str(rating), str(cve), str(bounty), str(upvotes)))    
-                    self.CONN.commit()
-                    self.CONN.close()
-
-                ## Warn the user that there are undisclosed reports
+            ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
+            tags = str(item.find('a', class_='spec-hacktivity-item-title')).replace('<a class=\"daisy-link routerlink daisy-link hacktivity-item__publicly-disclosed spec-hacktivity-item-title\" href=\"', '').replace('\">', '').replace('</a>', '').replace('</strong>', '').split('<strong>')
+            for tag in tags:
+                if index == 1:
+                    title = str(tag).strip().replace(',', '')
                 else:
-                    self.UNDISCLOSED_EXISTS = True
+                    report_number = str(tag).split('/')[-1].replace(',', '')
+                    url = str('https://hackerone.com' + tag)
+                index += 1
+
+            if ('CVE-' in title):
+                cve = title.split('CVE-')[1].split(' ')[0].replace(':', '').replace(')', '')
+                cve = 'CVE-' + cve
+            
+            ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
+            rating = str(item.find('div', class_='spec-severity-rating')).replace('<div class=\"sc-bcXHqe NcSfA daisy-severity-label spec-severity-rating\">', '').replace('</div>', '')
+
+            ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
+            bounty = str(item.find('strong', class_='spec-hacktivity-item-bounty')).replace('<strong class=\"spec-hacktivity-item-bounty\">', '').replace('</strong>', '').replace(',', '')
+            if str(bounty) == '':
+                bounty = '0.00'
+
+            ## Javascript DOM broke BeautifulSoup so I had to use Garbage Code to get the data
+            upvotes = str(item.find('span', class_='inline-help')).replace('<span class=\"inline-help\" style=\"display: inline-flex;\">', '').replace('</span>', '').replace(',', '')
+            if str(upvotes) == '':
+                upvotes = '0'
+
+            ## Publish the information for disclosed reports
+            if title != '':
+                _report['title'] = str(title)
+                _report['report number'] = str(report_number).replace('" rel="" target="', '')
+                _report['url'] = str(url)
+                _report['severity rating'] = rating
+                _report['bounty'] = str(bounty).replace('$', '').replace(',', '')
+                _report['upvotes'] = str(upvotes)
+
+                self.CONN = sql.connect('h1.db')
+                self.DATABASE['reports'].append(_report)
+                self.CONN.execute("INSERT INTO REPORTS (TITLE,REPORT_NUMBER,URL,SEVERITY_RATING,CVE,BOUNTY,UPVOTES) VALUES (?, ?, ?, ?, ?, ?, ?)", (str(title), str(report_number), str(url), str(rating), str(cve), str(bounty), str(upvotes)))    
+                self.CONN.commit()
+                self.CONN.close()
+
+            ## Warn the user that there are undisclosed reports
+            else:
+                self.UNDISCLOSED_EXISTS = True
 
         self.driver.quit()
 
@@ -252,7 +248,7 @@ if __name__ == "__main__":
     argv.add_argument("-k", "--key_word", default="", help="Key word to search for <api>")
     argv.add_argument("-o", "--order", default="popular", help="Order to sort by <popular | new>")
     argv.add_argument("-t", "--type", default="public", help="<all | bounty-awarded | hacker-published | public>")
-    argv.add_argument("-f", "--file_out", default="public", help="<name and extension of the file to create>")
+    argv.add_argument("-f", "--file_out", default="outfile_db.json", help="<name and extension of the file to create>")
 
     parser = argv.parse_args()
     source = parser.source
